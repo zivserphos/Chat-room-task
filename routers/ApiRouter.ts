e55954fda2e014b3ff4c798aeca8a86b00581674
zivserphos <zivfromisrael@gmail.com>
2022-01-02 17:05:20 +0200

    Revert "deleted unneccesary files"
    This reverts commit 393fb0b4d9d106d7b4701f29053f3b05f68e3d55.

--- routers/ApiRouter.ts
+++ routers/ApiRouter.ts
@@ -1,4 +1,12 @@
 import express from "express";
 const router = express.Router();
 
+// import { postComment, chatStream, onlineUser } from "../controllers/chatRoom";
+
+// router.get("/chatStream", chatStream);
+
+// router.post("/postComment", postComment);
+
+// router.post("/addOnlineUser/:userName", onlineUser);
+
 export default router;
