34c72b097177f6ef8aa41d220fc1576acd9f48f0
zivserphos <zivfromisrael@gmail.com>
2022-01-02 17:30:13 +0200

    test3

--- build/controllers/staticFiles.js
+++ build/controllers/staticFiles.js
@@ -6,6 +6,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.homePage = void 0;
 const path_1 = __importDefault(require("path"));
 const homePage = (_req, res) => {
-    res.sendFile(path_1.default.resolve("../client/build/index.html"));
+    res.sendFile(path_1.default.resolve("./client/build/index.html"));
 };
 exports.homePage = homePage;
